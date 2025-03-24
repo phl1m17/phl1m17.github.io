f8466bd3146ae5553e131b03fa4acddd79ba8c36
philm17 <clarkmanag@gmail.com>
2025-03-24 13:58:54 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -15,11 +15,3 @@ image.addEventListener('click', () => {
         content.classList.add('show');
     }
 });
-
-function openModal(src) {
-    document.getElementById("modal").style.display = "block";
-    document.getElementById("modal-img").src = src;
-}
-function closeModal() {
-    document.getElementById("modal").style.display = "none";
-}
