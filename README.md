2e35014a0c1e00801ab8bdec4d3368f9038fbcbb
phl1m17 <clarkmanag@gmail.com>
2025-06-15 01:37:05 -0400

    a

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Website Setup
+
+This project uses React with TypeScript and Vite for a fast and smooth development experience. Hot Module Replacement (HMR) is enabled so changes show up instantly while you’re coding.
---
+++ src/pages/Home/ExtracurricularsSection.tsx
@@ -0,0 +1,58 @@
+function ExtracurricularsSection() {
+  return (
+    <section>
+      <hr />
+      <h2>Extracurriculars</h2>
+      <p>
+        I’m actively involved in several clubs including GeoGuessr (
+        <span
+          style={{
+            color: "#ff6f61",
+            fontWeight: 600,
+            fontStyle: "italic",
+          }}
+        >
+          President
+        </span>
+        ), Table Tennis (
+        <span
+          style={{
+            color: "#ff6f61",
+            fontWeight: 600,
+            fontStyle: "italic",
+          }}
+        >
+          President
+        </span>
+        ), Computer Science (
+        <span
+          style={{
+            color: "#f5a623",
+            fontWeight: 600,
+            fontStyle: "italic",
+          }}
+        >
+          Vice President
+        </span>
+        ), and Software Development (
+        <span
+          style={{
+            color: "#4a90e2",
+            fontWeight: 600,
+            fontStyle: "italic",
+          }}
+        >
+          Social Media
+        </span>
+        ).
+      </p>{" "}
+      <br />
+      <p>
+        In competitions, I’ve earned 2nd place at the UofT Space Exploration &
+        Engineering Kompetition, and a Bronze Award from IOAI Canada.
+      </p>
+    </section>
+  );
+}
+
+export default ExtracurricularsSection;
