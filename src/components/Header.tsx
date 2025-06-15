095acd965a1a0256b1220ffb98e1dbcf8ce30b89
phl1m17 <clarkmanag@gmail.com>
2025-06-15 00:38:26 -0400

    ONLY MOBILE SO FAR

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <Link to="/">phl1m17</Link>
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/Photography/PhotographyPage.tsx
@@ -0,0 +1,11 @@
+function PhotographyPage() {
+  return (
+    <section>
+      <hr />
+      <h2>Photography</h2>
+      <p>Here are some of the cool things I’ve built!</p>
+    </section>
+  );
+}
+
+export default PhotographyPage;
