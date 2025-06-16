a87682a48b10d3af8d3c99685f15d1fc884061ef
phl1m17 <clarkmanag@gmail.com>
2025-06-15 20:28:37 -0400

    yuh

--- src/pages/Home/AboutSection.tsx
+++ src/pages/Home/AboutSection.tsx
@@ -6,9 +6,9 @@ function AboutSection() {
       <hr />
       <h2>About Me</h2>
       <p>
-        Hi, I’m an {age} year old student interested in software development,
-        programming, game development, and photography. This website showcases
-        my projects and passions across these areas.
+        Hi, I’m Phil, an {age} year old student passionate about software
+        development, programming, game development, and photography. This
+        website showcases my projects and passions across these areas.
       </p>
     </section>
   );
