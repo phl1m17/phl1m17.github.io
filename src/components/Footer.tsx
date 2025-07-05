f4a3f0d5a739919d8740d2ddfda4091bbc7ee062
phl1m17 <clarkmanag@gmail.com>
2025-07-04 20:40:34 -0400

    update footer + extracurricular

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaEnvelope, FaInstagram } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 
 import "./Footer.css";
@@ -9,22 +9,22 @@ function Footer() {
       <hr />
       <div className="social-icons">
         <a
-          href="https://www.linkedin.com/in/phil-clarence-m-581b45335/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="icon-link"
-          aria-label="Linkedin"
-        >
-          <FaLinkedin />
-        </a>
-        <a
           href="https://github.com/phl1m17"
           target="_blank"
           rel="noopener noreferrer"
           className="icon-link"
           aria-label="GitHub"
         >
-          <FaGithub />
+          <FaGithub size={30} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/phil-clarence-m-581b45335/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon-link"
+          aria-label="Linkedin"
+        >
+          <FaLinkedin size={30} />
         </a>
         <a
           href="https://leetcode.com/u/phl1m17/"
@@ -33,14 +33,23 @@ function Footer() {
           className="icon-link"
           aria-label="LeetCode"
         >
-          <SiLeetcode />
+          <SiLeetcode size={30} />
         </a>
         <a
           href="mailto:hello@philm.dev"
           className="icon-link"
           aria-label="Email"
         >
-          <FaEnvelope />
+          <FaEnvelope size={30} />
+        </a>
+        <a
+          href="https://www.instagram.com/philm_17/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon-link"
+          aria-label="Instagram"
+        >
+          <FaInstagram size={30} />
         </a>
       </div>
       <p>© {new Date().getFullYear()} Phil. All rights reserved.</p>
--- src/pages/Home/ExtracurricularsSection.tsx
+++ src/pages/Home/ExtracurricularsSection.tsx
@@ -3,54 +3,67 @@ function ExtracurricularsSection() {
     <section>
       <hr />
       <h2>Extracurriculars</h2>
-      <p>
-        I’m actively involved in several clubs including GeoGuessr (
-        <span
-          style={{
-            color: "#ff6f61",
-            fontWeight: 300,
-            fontStyle: "italic",
-          }}
-        >
-          President
-        </span>
-        ), Table Tennis (
-        <span
-          style={{
-            color: "#ff6f61",
-            fontWeight: 300,
-            fontStyle: "italic",
-          }}
-        >
-          President
-        </span>
-        ), Computer Science (
-        <span
-          style={{
-            color: "#f5a623",
-            fontWeight: 300,
-            fontStyle: "italic",
-          }}
-        >
-          Vice President
-        </span>
-        ), and Software Development (
-        <span
-          style={{
-            color: "#4a90e2",
-            fontWeight: 300,
-            fontStyle: "italic",
-          }}
-        >
-          Social Media
-        </span>
-        ).
-      </p>{" "}
+      <div>
+        <h3>University</h3>
+        <p>
+          I haven’t started university yet, so I don’t have any extracurriculars
+          at the moment, but I plan to get involved once the academic year
+          begins.
+        </p>
+      </div>
       <br />
-      <p>
-        In competitions, I’ve earned 2nd place at the UofT Space Exploration &
-        Engineering Kompetition, and a Bronze Award from IOAI Canada.
-      </p>
+      <div>
+        <h3>High School</h3>
+        <p>
+          I was actively involved in several clubs during high school, including
+          GeoGuessr (
+          <span
+            style={{
+              color: "#ff6f61",
+              fontWeight: 300,
+              fontStyle: "italic",
+            }}
+          >
+            President
+          </span>
+          ), Table Tennis (
+          <span
+            style={{
+              color: "#ff6f61",
+              fontWeight: 300,
+              fontStyle: "italic",
+            }}
+          >
+            President
+          </span>
+          ), Computer Science (
+          <span
+            style={{
+              color: "#f5a623",
+              fontWeight: 300,
+              fontStyle: "italic",
+            }}
+          >
+            Vice President
+          </span>
+          ), and Software Development (
+          <span
+            style={{
+              color: "#4a90e2",
+              fontWeight: 300,
+              fontStyle: "italic",
+            }}
+          >
+            Social Media
+          </span>
+          ).
+        </p>{" "}
+        <br />
+        <p>
+          In competitions, I’ve earned 2nd place at the UofT Space Exploration &
+          Engineering Kompetition, and a Bronze Award from IOAI Canada.
+        </p>
+      </div>
     </section>
   );
 }
