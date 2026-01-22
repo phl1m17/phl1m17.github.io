5ffdcd070ad30dc2de83089a2bb88d01e4698614
philm <clarkmanag@gmail.com>
2026-01-22 00:09:16 -0500

    Fix typo in AboutSection introduction

--- src/pages/Home/AboutSection.tsx
+++ src/pages/Home/AboutSection.tsx
@@ -6,7 +6,7 @@ function AboutSection() {
       <hr />
       <h2>About Me</h2>
       <p>
-        Hi, I’m Phil, an {age} year old student passionate about software
+        Hi, I’m Phil, a {age} year old student passionate about software
         development, programming, game development, and photography. This
         website showcases my projects and passions across these areas.
       </p>
